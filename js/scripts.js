0fdb3a84698209635e872c1ca5f90345a074603f
alanbjordan <alan.jordanbiz@gmail.com>
2020-03-20 18:45:52 -0400

    updates

--- js/scripts.js
+++ js/scripts.js
@@ -12,12 +12,6 @@ $(function() {
     });
 });
 
-
-// $(function(){
-//     $('a[href^=#]').click(function(e){
-//       var name = $(this).attr('href').substr(1);
-//       var pos = $('a[name='+name+']').offset();
-//       $('body').animate({ scrollTop: pos.top });
-//       e.preventDefault();
-//     });
-//   });
+$( "#section5" ).scroll(function() {
+    $("#imageToAppear").fadeIn();
+   });
