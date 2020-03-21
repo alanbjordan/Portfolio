3b50974afeddadf3dcea0c1140da983ecb79b433
alanbjordan <alan.jordanbiz@gmail.com>
2020-03-20 20:41:03 -0400

    added resume updates

--- js/scripts.js
+++ js/scripts.js
@@ -11,13 +11,3 @@ $(function() {
         }
     });
 });
-
-
-// $(function(){
-//     $('a[href^=#]').click(function(e){
-//       var name = $(this).attr('href').substr(1);
-//       var pos = $('a[name='+name+']').offset();
-//       $('body').animate({ scrollTop: pos.top });
-//       e.preventDefault();
-//     });
-//   });
